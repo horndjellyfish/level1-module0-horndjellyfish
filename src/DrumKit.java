99ca14df39b164f5d238c8ca74910b661e5d4fbf
paradisefall123 <league@Admins-Mac-mini.local>
2018-03-24 11:27:29 -0700

    MyFirstSwingGUI - finished, PhotoQuiz - finished, DrumKit - unfinished
    Day 1

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -28,25 +28,25 @@ public class DrumKit extends MouseAdapter {
 
     private void getGoing() throws MalformedURLException {
    	 // 1. Make a JFrame variable and initialize it using "new JFrame()"
-   	 
+   	 JFrame frame = new JFrame();
    	 // 2. Make the frame visible
-   	 
+   	 frame.setVisible(true);
    	 // 3. Set the size of the frame
-   	 
+   	 frame.setSize(200, 200);
    	 // 4. Set the title of the frame
-   	 
+   	 frame.setTitle("Title");
    	 // 5. Make a JPanel and initialize it.
-   	 
+   	 JPanel panel = new JPanel();
    	 // 6. Add the panel to the frame. (The panel is invisible.)
-    
+   	 frame.add(panel);
    	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
    	 // 8. Put the name of your image in a String variable.
-    
+     String image = "drumkit.jpg";
    	 // 9. Edit the next line to use your String variable
-// drumLabelWithImage = createLabelImage(drumImageString);
+     drumLabelWithImage = createLabelImage(drumImageString);
    	 
    	 // 10. Add the image to the panel
-    
+     
    	 // 11. Set the layout of the panel to "new GridLayout()"
     
    	 // 12. call the pack() method on the frame
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,33 +22,44 @@ public class PhotoQuiz {
 		quizWindow.setVisible(true);
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image URL”)
-
+		String image = "https://static1.squarespace.com/static/552dd168e4b0e28fdcecdbcf/57bb7c0a3e00be03ee97fb38/57bb7c2615d5dbc1c52fb350/1471904807951/Rhett+and+Link.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component comp;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		comp = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(comp);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String ans = JOptionPane.showInputDialog("Which one is taller, Rhett or Link?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (ans.equalsIgnoreCase("Rhett")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		quizWindow.remove(comp);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String secimage = "http://images.amcnetworks.com/bbcamerica.com/wp-content/uploads/2014/04/davidtennant-1600x720.jpg";
+		comp = createImage(secimage);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(comp);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String answ = JOptionPane.showInputDialog("Which Doctor is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if (answ.equals("10")|| answ.equalsIgnoreCase("Tenth")|| answ.equalsIgnoreCase("Ten")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 	}
 
 	private static Component createImage(String imageUrl) throws MalformedURLException {
